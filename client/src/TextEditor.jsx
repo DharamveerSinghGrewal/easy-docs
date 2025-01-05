e45b5a23bb0dd4980d1ee96c52cc5bb78c5446a8
DharamveerGrewal <135950075+DharamveerGrewal@users.noreply.github.com>
2025-01-04 21:56:20 -0500

    Integrated Quill Text Editor

--- client/src/TextEditor.jsx
+++ client/src/TextEditor.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useCallback} from 'react'
+import Quill from 'quill'
+import "quill/dist/quill.snow.css"
 
 export default function TextEditor() {
-  return (
-    <div>
-      Quill implementation here
-    </div>
-  )
+  const setupQuillDOM
+   = useCallback(quillWrapper=>{
+  
+  if(quillWrapper== null) return
+
+  //Remove any existing Quill DOMs to avoid duplicates
+  quillWrapper.innerHTML = ''
+
+  //create a new div to act as the container for Quill instance
+  const quillContainer = document.createElement('div')
+  quillWrapper.append(quillContainer)
+
+  // Initialize Quill editor instance in the container with the "snow" theme
+  new Quill(quillContainer, {theme: "snow"})
+  },[])
+
+  return <div id="quillWrapper" ref={setupQuillDOM
+
+  }> </div>
+  
 }
