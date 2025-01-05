71578d84113d2dfb09ba29e8bef7d82f44a6bcd4
DharamveerGrewal <135950075+DharamveerGrewal@users.noreply.github.com>
2025-01-05 13:44:50 -0500

    Add toolbar options for quill editor

--- client/src/TextEditor.jsx
+++ client/src/TextEditor.jsx
@@ -2,6 +2,21 @@ import React, { useCallback} from 'react'
 import Quill from 'quill'
 import "quill/dist/quill.snow.css"
 
+const TOOLBAR_OPTIONS = [
+[{size: [ 'small', false, 'large', 'huge' ]}],
+[{font: []}],
+[{list: "ordered"}, {list: "bullet"},{ 'list': 'check' }],
+["bold", "italic", "underline","strike"],
+[{color: []}, {background: []}],
+[{script: "sub"}, {script: "super"}],
+[{ 'indent': '-1'}, { 'indent': '+1' }], 
+[{align: []}],
+["image", "blockquote", "code-block"],
+[{align: []}],
+["image", "blockquote", "code-block"],
+["clean"],
+
+]
 export default function TextEditor() {
   const setupQuillDOM
    = useCallback(quillWrapper=>{
@@ -16,7 +31,7 @@ export default function TextEditor() {
   quillWrapper.append(quillContainer)
 
   // Initialize Quill editor instance in the container with the "snow" theme
-  new Quill(quillContainer, {theme: "snow"})
+  new Quill(quillContainer, {theme: "snow", modules: {toolbar: TOOLBAR_OPTIONS}})
   },[])
 
   return <div className="quillWrapper" ref={setupQuillDOM}> </div>
