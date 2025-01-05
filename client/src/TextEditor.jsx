6ff6e4ebe3384586a69192b2062fb661e6c4b85f
DharamveerGrewal <135950075+DharamveerGrewal@users.noreply.github.com>
2025-01-05 15:09:09 -0500

    Implement real-time text changes with socket.io - Emit text changes from client to server - Broadcast text changes from server to all clients - Receive and display broadcasted changes on all clients

--- client/src/TextEditor.jsx
+++ client/src/TextEditor.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect} from 'react'
+import React, { useCallback, useEffect, useState} from 'react'
 import Quill from 'quill'
 import "quill/dist/quill.snow.css"
 import {io} from "socket.io-client"
@@ -20,14 +20,39 @@ const TOOLBAR_OPTIONS = [
 ]
 export default function TextEditor() {
 
-  
+  const [socket, setSocket] = useState()
+  const [quill, setQuill] = useState()
   useEffect(() =>{
-    const socket = io("http://localhost:3001")
+    const server = io("http://localhost:3001")
+    setSocket(server)
     return () =>{
-        socket.disconnect()
+        server.disconnect()
     }
   }, [])
 
+  useEffect(() => {
+    if(socket ==null || quill == null) return
+    const updateDocument = (delta) =>{
+       quill.updateContents(delta)
+    }
+    socket.on('receive-changes', updateDocument)
+    return() => {
+        socket.off('receive-changes', updateDocument)
+    }
+   }, [socket, quill])
+
+   useEffect(() => {
+    if(socket ==null || quill == null) return
+    const updateDocument = (delta, oldDelta, source) =>{
+        if(source !== 'user') return //only send user made changes to the server
+        socket.emit("send-changes", delta)
+
+    }
+    quill.on('text-change', updateDocument)
+    return() => {
+        quill.off('text-change', updateDocument)
+    }
+   }, [socket, quill])
   const setupQuillDOM
    = useCallback(quillWrapper=>{
   
@@ -41,7 +66,8 @@ export default function TextEditor() {
   quillWrapper.append(quillContainer)
 
   // Initialize Quill editor instance in the container with the "snow" theme
-  new Quill(quillContainer, {theme: "snow", modules: {toolbar: TOOLBAR_OPTIONS}})
+  const quill_instance = new Quill(quillContainer, {theme: "snow", modules: {toolbar: TOOLBAR_OPTIONS}})
+  setQuill(quill_instance)
   },[])
 
   return <div className="quillWrapper" ref={setupQuillDOM}> </div>
--- server/server.js
+++ server/server.js
@@ -8,5 +8,7 @@ const io = require('socket.io')(3001, {
 })
 
 io.on("connection", socket =>{
-    console.log("Connected to Server successfully!")
+    socket.on("send-changes", delta => {
+        socket.broadcast.emit("receive-changes", delta)
+    })
 })
